06e43b18caf34077eea138785ccb907be2acf9e5
radical81 <ralobba@gmail.com>
2017-06-14 21:23:18 +1000

    Initial Commit

---
+++ CarPark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarPark
+//
+//  Created by Rex Jason Alobba on 14/6/17.
+//  Copyright © 2017 Rex Jason Alobba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
